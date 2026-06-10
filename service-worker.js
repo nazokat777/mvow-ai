bb851b25f419e2303fc8769f3399128e23ad74f5
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-10 08:01:27 +0500

    v7.2: today-plan editable (qo'shish/tahrirlash/o'chirish + localStorage)

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'mvow-v7.1.0';
+const CACHE_NAME = 'mvow-v7.2.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
