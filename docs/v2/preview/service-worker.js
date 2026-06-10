e232ccd5139677625aee48559e11bde7c69d0f0f
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-10 20:00:57 +0500

    v11.1 toza boshlash: soxta 12-kun / 38-soat / 5-ish namunalari olib tashlandi. Yangi foydalanuvchi 0 dan boshlaydi, hech narsa o'zi sehrli paydo bo'lmaydi

--- docs/v2/preview/service-worker.js
+++ docs/v2/preview/service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'mvow-v11.0.0';
+const CACHE_NAME = 'mvow-v11.1.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
