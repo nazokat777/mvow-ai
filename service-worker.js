c1f1cc35a15e68aa11c719cea2e1ab36b3c5e96a
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-10 07:39:20 +0500

    v6.3: welcome animatsiya + aniq va'da matni + goal profil (yosh/jins/ish/niyat) + data.profile

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'mvow-v6.2.0';
+const CACHE_NAME = 'mvow-v6.3.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
