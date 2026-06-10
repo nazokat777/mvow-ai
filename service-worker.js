9d2beb7a9e8a3133de319b4e2f17bc9bcc43595a
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-10 08:04:38 +0500

    v7.3: routine'da ovozli kiritish (Web Speech API)

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'mvow-v7.2.0';
+const CACHE_NAME = 'mvow-v7.3.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
