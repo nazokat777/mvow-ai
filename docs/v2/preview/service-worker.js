39169e28c59c982c4f597f239ce57d06f698a90b
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-20 08:48:47 +0500

    fix: taymer nomi mustahkam (maqsad nomidan zaxira) + bajargandan keyin izoh ko'rsatiladi (saqlanганiga tasdiq) + SW kesh v28.5.1

--- docs/v2/preview/service-worker.js
+++ docs/v2/preview/service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'daywarden-v28.5.0';
+const CACHE_NAME = 'daywarden-v28.5.1';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
