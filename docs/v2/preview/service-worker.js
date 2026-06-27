0f0ed89d175c715340bc9d4d11089f41376d553d
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-27 21:29:32 +0500

    feat: maqsadlar bo'sh kartasi bosiladigan (->maqsad qo'shish) + bo'sh holatda 'Yangi maqsad' yashirin (takror emas); maqsad qo'shishda 'Misollar' ro'yxati olib tashlandi (sodda); SW v28.22.0

--- docs/v2/preview/service-worker.js
+++ docs/v2/preview/service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'daywarden-v28.21.0';
+const CACHE_NAME = 'daywarden-v28.22.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
