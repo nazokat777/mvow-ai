a76bd6786642d27418a29ac0b8fc02ba4036e9f9
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-27 21:55:41 +0500

    style: 'intizom do'stingiz' tagline kirish/launcher/app'dan olib tashlandi (sodda); SW v28.26.0

--- docs/v2/preview/service-worker.js
+++ docs/v2/preview/service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'daywarden-v28.25.0';
+const CACHE_NAME = 'daywarden-v28.26.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
