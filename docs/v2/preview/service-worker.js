0ae07b89ea2a0925f36606aac041b59fc44a58fe
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-28 04:33:36 +0500

    style: siyohrang (indigo) urg'ular -> ORANGE butun ilovada (18 fayl); rang-barang kartalar saqlandi; maqsadlar kartalari ham rang-barang; SW v28.42.0

--- docs/v2/preview/service-worker.js
+++ docs/v2/preview/service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'daywarden-v28.41.0';
+const CACHE_NAME = 'daywarden-v28.42.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
