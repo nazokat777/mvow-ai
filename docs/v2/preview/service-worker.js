9c0a59bbac6374dbf6d576063c66dabd77837b26
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-29 00:50:34 +0500

    fix(hard-lock): fokus boshlanganda nav kapsulasi + til tugmasi yashiriladi (immersiv toza ekran; qorong'idagi oq blok yo'qoldi); SW v28.60.0

--- docs/v2/preview/service-worker.js
+++ docs/v2/preview/service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'daywarden-v28.59.0';
+const CACHE_NAME = 'daywarden-v28.60.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
