212648b2de19272780f770b8204f969781b561a6
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-06-30 16:41:49 +0500

    feat(fon boshqaruvi): rasm yoqmasa -> '🔄 Boshqasi' (yangi tasodifiy foto) + 'Fonsiz' (umuman o'chirish, toggle); 'Rasm' = o'z rasmini yuklash. Holatlar: avto/o'z/o'chiq (saqlanadi). i18n 3 til; SW focusai-v28.91.0

--- docs/v2/preview/service-worker.js
+++ docs/v2/preview/service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'focusai-v28.90.0';
+const CACHE_NAME = 'focusai-v28.91.0';
 const ASSETS = [
   './',
   // Asosiy infratuzilma
